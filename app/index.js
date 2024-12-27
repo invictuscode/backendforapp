ce1daa9bcf9aa20753c94cbf2e6e52261890ea7e
Aaditya K <akkool2010@gmail.com>
2024-12-27 13:39:36 -0500

    port fixed

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
